7328b92436ab54ab7389731c102cf705ee9a8331
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2023-12-23 20:12:57 +0200

    finish third section

--- demoproject/src/Pages/FirstPage.js
+++ demoproject/src/Pages/FirstPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TopTemplate from '../Templates/TopTemplate';
-
+import ThirdTemplate from '../Templates/ThirdTemplate';
 const FirstPage = () => {
     return (
       <div>
         <TopTemplate/>
+        <ThirdTemplate/>
       </div>
     );
   }
---
+++ demoproject/src/Templates/ThirdTemplate.js
@@ -0,0 +1,11 @@
+import ThirdSection from '../Organisms/ThirdSection';
+
+const ThirdTemplate = () => {
+    return (
+      <div>
+         <ThirdSection/>
+      </div>
+    );
+  }
+  
+  export default ThirdTemplate;
