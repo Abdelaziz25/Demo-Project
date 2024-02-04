480b4577715b740cbbcb8a6a62902e28af3edd9e
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2024-02-04 21:08:24 +0200

    handle some spacing

--- demoproject/src/Molecules/Cards/card2.js
+++ demoproject/src/Molecules/Cards/card2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './card2.css'; 
+import '../Cards/card2.css'; 
 import Google from '../../images/ibm.png'
 const Card2 = () => {
   return (
@@ -7,8 +7,11 @@ const Card2 = () => {
     <div className="upper-part">
       <img src={Google} alt="certificate" className="centered-image" />
     </div>
-    <div className="lower-part">
-      Google IT Support Professional Certificate
+    <div className="lower-part lower2-part">
+      Google IT Support 
+    </div>
+    <div className="lower-part lower3-part">
+    Professional Certificate
     </div>
     <div className="google-text">
       Google
